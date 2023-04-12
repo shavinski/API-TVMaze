d471efb9e5385f5d13a930a9bf50d8be72bce58d
Ryan Park <ryrypark@gmail.com>
2023-04-12 12:18:12 -0700

    get show api data complete

--- tvmaze.js
+++ tvmaze.js
@@ -3,6 +3,7 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const TV_MAZE_URL = "http://api.tvmaze.com"
 
 
 /** Given a search term, search for tv shows that match that query.
@@ -12,27 +13,18 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(term) {
+  const show = $("#searchForm-term").val();
+  const showData = await axios.get(`${TV_MAZE_URL}/search/shows`, {params: {
+    q: show
+    }
+  })
+  console.log("showData=", showData);
+
   // ADD: Remove placeholder & make request to TVMaze search shows API.
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  // return [
+  // ]
 }
 
 
@@ -48,9 +40,9 @@ function displayShows(shows) {
     const $show = $(`
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -97,3 +89,19 @@ $searchForm.on("submit", async function handleSearchForm (evt) {
 // function displayEpisodes(episodes) { }
 
 // add other functions that will be useful / match our structure & design
+// {
+    //   id: 1767,
+    //   name: "The Bletchley Circle",
+    //   summary:
+    //     `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
+    //        women with extraordinary skills that helped to end World War II.</p>
+    //      <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+    //        normal lives, modestly setting aside the part they played in
+    //        producing crucial intelligence, which helped the Allies to victory
+    //        and shortened the war. When Susan discovers a hidden code behind an
+    //        unsolved murder she is met by skepticism from the police. She
+    //        quickly realises she can only begin to crack the murders and bring
+    //        the culprit to justice with her former friends.</p>`,
+    //   image:
+    //       "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+    // }
